7afe05d476f5c4288174009d25981dc06f9c47d0
zakhavai@gmail.com <zakhavai@gmail.com>
2024-01-30 12:05:38 +0300

    test

--- netlify/functions/bot.ts
+++ netlify/functions/bot.ts
@@ -2,12 +2,34 @@ import { config } from 'dotenv';
 import TelegramBot from 'node-telegram-bot-api';
 
 config();
-// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_BOT_TOKEN || '';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
-  
-bot.on('message', (msg) => {
-    bot.sendMessage(msg.chat.id, 'test')  
-})
+
+export async function handler(event: any, context: any) {
+  try {
+    const body = JSON.parse(event.body);
+
+    if (
+      body &&
+      body.message &&
+      body.message.text &&
+      body.message.text.toLowerCase().includes('/testcommand')
+    ) {
+      const chatId = body.message.chat.id;
+      await bot.sendMessage(chatId, 'This is a response to the /testcommand!');
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Processed successfully' }),
+    };
+  } catch (error) {
+    console.error('Error handling Telegram message:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+}
