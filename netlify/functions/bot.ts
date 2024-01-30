6ccad041486674b84f3af4c0066e373ba4ae4b0a
zakhavai@gmail.com <zakhavai@gmail.com>
2024-01-30 11:43:37 +0300

    test

--- netlify/functions/bot.ts
+++ netlify/functions/bot.ts
@@ -1,7 +1,13 @@
-exports.handler = async () => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello, Netlify Function!" }),
-    };
-  };
+import { config } from 'dotenv';
+import TelegramBot from 'node-telegram-bot-api';
+
+config();
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_BOT_TOKEN || '';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
   
+bot.on('message', (msg) => {
+    bot.sendMessage(msg.chat.id, 'test')  
+})
