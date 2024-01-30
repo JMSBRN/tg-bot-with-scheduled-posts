24df39e6d57dfb3d4c373004c73e7e2919604684
zakhavai@gmail.com <zakhavai@gmail.com>
2024-01-30 17:02:47 +0300

    add weather api call

---
+++ netlify/functions/weather.ts
@@ -0,0 +1,32 @@
+// https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=692558f436b41d827e09484ce4725e62
+
+
+import { Handler, HandlerContext, HandlerEvent } from '@netlify/functions';
+import fetch from 'node-fetch';
+
+const apiToken = process.env.WEATHER_API_ID;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${apiToken}`;
+
+const handler: Handler = async (event: HandlerEvent, cxt: HandlerContext) => {
+  try {
+    const apiRes = await fetch(apiUrl);
+
+    if (!apiRes.ok) {
+      throw new Error(`Weather API request failed with status ${apiRes.status}`);
+    }
+
+    const data = await apiRes.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
+
+export { handler };
