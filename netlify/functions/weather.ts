af5d0b85af0670fb17c8ae696ffffd5234c347b2
zakhavai@gmail.com <zakhavai@gmail.com>
2024-01-31 13:50:31 +0300

    test

--- netlify/functions/weather.ts
+++ netlify/functions/weather.ts
@@ -1,22 +1,20 @@
 import { Handler, HandlerContext, HandlerEvent } from '@netlify/functions';
-import { RequestInfo, RequestInit } from "node-fetch";
 
-const fetch = (url: RequestInfo, init?: RequestInit) =>  import("node-fetch").then(({ default: fetch }) => fetch(url, init));
+const fetch = import('node-fetch').then(({ default: fetch }) => fetch);
 
 const apiToken = process.env.WEATHER_API_ID;
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin,de&mode=html&units=metric&lang=ru&&APPID=${apiToken}`;
 
-console.log(apiUrl);
-
 const handler: Handler = async (event: HandlerEvent, cxt: HandlerContext) => {
   try {
-    const apiRes = await fetch(apiUrl);
+    const apiRes = (await fetch)(apiUrl);
 
-    if (!apiRes.ok) {
-      throw new Error(`Weather API request failed with status ${apiRes.status}`);
+    if (!(await apiRes).ok) {
+      const errorText = (await apiRes).text();
+      throw new Error(`Weather API request failed with status ${(await apiRes).status}. Error: ${errorText}`);
     }
 
-    const data = await apiRes.text();
+    const data = await (await apiRes).text();
     return {
       statusCode: 200,
       headers: { 'Content-type': 'text/html' },
@@ -32,3 +30,4 @@ const handler: Handler = async (event: HandlerEvent, cxt: HandlerContext) => {
 };
 
 export { handler };
+
