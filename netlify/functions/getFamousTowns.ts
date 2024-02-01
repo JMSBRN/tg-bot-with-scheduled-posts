90e619f1227e18ed625035b2613e106a043cac1b
zakhavai@gmail.com <zakhavai@gmail.com>
2024-02-01 12:49:24 +0300

    feat: cretaed  simple logic in home page with serverles functions

---
+++ netlify/functions/getFamousTowns.ts
@@ -0,0 +1,22 @@
+import { Handler, HandlerContext, HandlerEvent } from '@netlify/functions';
+import townsData from '../../src/famous-towns.json';
+
+const handler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext,
+) => {
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Success', towns: townsData }),
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
+
+export { handler };
