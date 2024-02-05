80d67c380346425ba7b61de3dccaa4b7f95366ac
zakhavai@gmail.com <zakhavai@gmail.com>
2024-02-05 22:06:06 +0300

    fix: add calendar api (not end)

--- netlify/functions/calendar.ts
+++ netlify/functions/calendar.ts
@@ -1,17 +1,50 @@
 import { Handler, HandlerContext, HandlerEvent } from '@netlify/functions';
+import { google } from 'googleapis';
+import { config } from 'dotenv';
+import { credentials } from '../../src/utils/google-calendar/credentials';
 
-const handler: Handler = async (event: HandlerEvent, cxt: HandlerContext) => {
+config();
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  const calendarId = process.env.CALENDAR_ID;
+
+  if (!calendarId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'CALENDAR_ID environment variable is not set.' }),
+    };
+  }
+
   try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: credentials.web,
+      scopes: ['https://www.googleapis.com/auth/calendar.readonly'],
+    });
+
+    const calendar = google.calendar({
+      version: 'v3',
+      auth: auth,
+    });
+
+    const { data } = await calendar.events.list({
+      calendarId: calendarId,
+      timeMin: new Date().toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+    });
+
     return {
       statusCode: 200,
       headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({ test: 'test calendar' }),
+      body: JSON.stringify({ data }),
     };
   } catch (error) {
-    console.error('Error fetching weather data:', error);
+    console.error('Error fetching calendar events:', error);
+
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
+      body: JSON.stringify({ error: (error as Error).message }),
     };
   }
 };
